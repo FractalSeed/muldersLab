5a30e8c80f031e67989d2a5d13b62b71d5bcb005
anand <anand@tappp.com>
2015-10-25 00:11:17 +0530

    Raster style display added

---
+++ Arcxelsimulator/src/mulderslab/ArcxelSimulator/ImageCompressor.java
@@ -0,0 +1,18 @@
+package mulderslab.ArcxelSimulator;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+
+public class ImageCompressor {
+	
+	public static ImageIcon resizeImageIcon( ImageIcon imageIcon , Integer width , Integer height )
+	{
+	    BufferedImage bufferedImage = new BufferedImage( width , height , BufferedImage.TRANSLUCENT );
+
+	    Graphics2D graphics2D = bufferedImage.createGraphics();
+	    graphics2D.drawImage( imageIcon.getImage() , 0 , 0 , width , height , null );
+	    graphics2D.dispose();
+
+	    return new ImageIcon( bufferedImage , imageIcon.getDescription() );
+	}
+}
--- Arcxelsimulator/src/mulderslab/ArcxelSimulator/Main.java
+++ Arcxelsimulator/src/mulderslab/ArcxelSimulator/Main.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
@@ -26,11 +27,16 @@ class Surface extends JPanel implements ActionListener {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private final int DELAY = 150;
+	private final int DELAY = 100;
     private Timer timer;
-    public Image image ;
+    public BufferedImage image ;
+    public static int sizex= 400;
+    public static int sizey = 400;
+    int lastx = 0, lasty = 0;
+    int c = 0;
     public Surface() throws IOException {
-    	image =  new ImageIcon("science.jpg").getImage();
+//    	image =  new ImageIcon("science.jpg").getImage();
+    	image =(BufferedImage) (ImageCompressor.resizeImageIcon(new ImageIcon("science.jpg"), new Integer(40), new Integer( 40)).getImage());
         initTimer();
     }
 
@@ -46,22 +52,46 @@ class Surface extends JPanel implements ActionListener {
     }
 
     private void doDrawing(Graphics g) {
-
+    	c = c+1;
+    	Color color = Color.WHITE;
+    	switch(c%3)
+    	{
+    	case 0:
+    		color = Color.RED;
+    		break;
+    	case 1: 
+    		color = Color.GREEN;
+    		break;
+    	case 2: 
+    		color = Color.BLUE;
+    		break;
+    	}
         Graphics2D g2d = (Graphics2D) g;
 
-        g2d.setPaint(Color.blue);
+        g2d.setPaint(color);
 
         int w = getWidth();
         int h = getHeight();
 
-        //Random r = new Random();
+        Random r = new Random();
 
-        for (int i = 0; i < 350; i++) {
-        	int x = Math.abs(i) % w;
-        	for(int j=0; j<250;j++)
+        for (int i = 0; i < w/10; i++) {
+        	int x = Math.abs(i) % w*10;
+        	for(int j=0; j<h/10;j++)
         	{
-        		int y = Math.abs(j) % h;
+        		int y =  Math.abs(j) % h*10;
+        		g2d.setPaint(Color.white);
+        		g2d.drawRect(x, y, 9, 9);
+        		
+        		int[][] result = convertTo2DUsingGetRGB(image);
+        		if(i<image.getHeight() && j < image.getWidth())
+        		color = new Color(result[i][j]);
+        		g2d.setPaint(color);
+        		g2d.fillRect(x, y, 9, 9);
+//        		if(i==c%40 && j ==i)
+//            		g2d.fillRect(x, (c%40) % h*10, 9, 9);
         	}
+        	
             
         }
 //        g2d.drawImage(image, 0, 0, null);
@@ -78,6 +108,21 @@ class Surface extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         repaint();
     }
+    
+    private static int[][] convertTo2DUsingGetRGB(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int[][] result = new int[height][width];
+
+        for (int row = 0; row < height; row++) {
+           for (int col = 0; col < width; col++) {
+              result[row][col] = image.getRGB(row, col);
+           }
+        }
+
+        return result;
+     }
+
 }
 
 public class Main extends JFrame {
@@ -106,7 +151,8 @@ public class Main extends JFrame {
         });
 
         setTitle("Points");
-        setSize(350, 250);
+        setSize(Surface.sizex, Surface.sizey);
+        setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
